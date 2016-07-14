9a3ff8d57ec07d2447b0cb19e1a7780110c47212
idenkim <mehoya92@naver.com>
2016-07-14 12:27:20 +0900

    test1

--- app/src/main/java/com/example/softheaven/sunjang_hangle/MainMenu.java
+++ app/src/main/java/com/example/softheaven/sunjang_hangle/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.softheaven.sunjang_hangle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -19,6 +20,8 @@ public class MainMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        Log.e("e", "log");
 
         waveAnimation();
     }
